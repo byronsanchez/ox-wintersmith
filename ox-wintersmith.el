ba2c742b1dc3a4a5264177bc65bc19eacd61b117
Byron Sanchez <byron@hackbytes.io>
2017-01-29 04:53:20 +0000

    Rename jekyll exporter to wintersmith

--- ox-wintersmith.el
+++ ox-wintersmith.el
@@ -1,23 +1,23 @@
-;;; ox-jekyll.el --- Export Jekyll articles using org-mode.
+;;; ox-wintersmith.el --- Export Wintersmith articles using org-mode.
 
 ;; Copyright (C) 2013  Yoshinari Nomura
 
 ;; Author: Yoshinari Nomura <nom@quickhack.net>
 ;; Author: Justin Gordon <justin.gordon@gmail.com>
-;; Keywords: org, jekyll
+;; Keywords: org, wintersmith
 ;; Version: 0.1
 
 ;;; Commentary:
 
-;; This library implements a Jekyll-style html backend for
+;; This library implements a Wintersmith-style html backend for
 ;; Org exporter, based on `html' back-end.
 ;;
 ;; It provides two commands for export, depending on the desired
 ;; output: `org-jkl-export-as-html' (temporary buffer) and
 ;; `org-jkl-export-to-html' ("html" file with YAML front matter).
 ;;
-;; For publishing, `org-jekyll-publish-to-html' is available.
-;; For composing, `org-jekyll-insert-export-options-template' is available.
+;; For publishing, `org-wintersmith-publish-to-html' is available.
+;; For composing, `org-wintersmith-insert-export-options-template' is available.
 
 ;;; Code:
 
@@ -27,13 +27,13 @@
 (defvar org-html-display-buffer-mode)
 ;;; User Configurable Variables
 
-(defgroup org-export-jekyll nil
-  "Options for exporting Org mode files to jekyll HTML."
-  :tag "Org Jekyll"
+(defgroup org-export-wintersmith nil
+  "Options for exporting Org mode files to wintersmith HTML."
+  :tag "Org Wintersmith"
   :group 'org-export
   :version "24.2")
 
-(defcustom org-jekyll-include-yaml-front-matter t
+(defcustom org-wintersmith-include-yaml-front-matter t
   "If true, then include yaml-front-matter when exporting to html.
 
 If false, then you should include the yaml front matter like this at the top of the file:
@@ -49,31 +49,31 @@ keywords: Octopress
 description: Instructions on Upgrading Octopress
 ---
 #+END_HTML"
-  :group 'org-export-jekyll
+  :group 'org-export-wintersmith
   :type 'boolean)
 
-(defcustom org-jekyll-layout "post"
-  "Default layout used in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-wintersmith-layout "post"
+  "Default layout used in Wintersmith article."
+  :group 'org-export-wintersmith
   :type 'string)
 
-(defcustom org-jekyll-categories ""
-  "Default space-separated categories in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-wintersmith-categories ""
+  "Default space-separated categories in Wintersmith article."
+  :group 'org-export-wintersmith
   :type 'string)
 
-(defcustom org-jekyll-published "true"
-  "Default publish status in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-wintersmith-published "true"
+  "Default publish status in Wintersmith article."
+  :group 'org-export-wintersmith
   :type 'string)
 
-(defcustom org-jekyll-comments ""
-  "Default comments (disqus) flag in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-wintersmith-comments ""
+  "Default comments (disqus) flag in Wintersmith article."
+  :group 'org-export-wintersmith
   :type 'string)
 
-(defcustom org-jekyll-use-src-plugin nil
-  "If t, org-jekyll exporter eagerly uses plugins instead of
+(defcustom org-wintersmith-use-src-plugin nil
+  "If t, org-wintersmith exporter eagerly uses plugins instead of
 org-mode's original HTML stuff. For example:
 
    #+BEGIN_SRC ruby
@@ -85,36 +85,36 @@ makes:
   {% highlight ruby %}
   puts \"Hello world\"
   {% endhighlight %}"
-  :group 'org-export-jekyll-use-src-plugin
+  :group 'org-export-wintersmith-use-src-plugin
   :type 'boolean)
 
 
 ;;; Define Back-End
 
-(org-export-define-derived-backend 'jekyll 'html
-  :export-block '("HTML" "JEKYLL")
+(org-export-define-derived-backend 'wintersmith 'html
+  :export-block '("HTML" "WINTERSMITH")
   :menu-entry
-  '(?j "Jekyll: export to HTML with YAML front matter."
-       ((?H "As HTML buffer" org-jekyll-export-as-html)
-        (?h "As HTML file" org-jekyll-export-to-html)))
+  '(?j "Wintersmith: export to HTML with YAML front matter."
+       ((?H "As HTML buffer" org-wintersmith-export-as-html)
+        (?h "As HTML file" org-wintersmith-export-to-html)))
   :translate-alist
-  '((template . org-jekyll-template) ;; add YAML front matter.
-    (src-block . org-jekyll-src-block)
-    (inner-template . org-jekyll-inner-template)) ;; force body-only
+  '((template . org-wintersmith-template) ;; add YAML front matter.
+    (src-block . org-wintersmith-src-block)
+    (inner-template . org-wintersmith-inner-template)) ;; force body-only
   :options-alist
-  '((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-layout)
-    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-categories)
-    (:jekyll-published "JEKYLL_PUBLISHED" nil org-jekyll-published)
-    (:jekyll-comments "JEKYLL_COMMENTS" nil org-jekyll-comments)))
+  '((:wintersmith-layout "WINTERSMITH_LAYOUT" nil org-wintersmith-layout)
+    (:wintersmith-categories "WINTERSMITH_CATEGORIES" nil org-wintersmith-categories)
+    (:wintersmith-published "WINTERSMITH_PUBLISHED" nil org-wintersmith-published)
+    (:wintersmith-comments "WINTERSMITH_COMMENTS" nil org-wintersmith-comments)))
 
 
 ;;; Internal Filters
-(defun org-jekyll-src-block (src-block contents info)
-  "Transcode SRC-BLOCK element into jekyll code template format
-if `org-jekyll-use-src-plugin` is t. Otherwise, perform as
+(defun org-wintersmith-src-block (src-block contents info)
+  "Transcode SRC-BLOCK element into wintersmith code template format
+if `org-wintersmith-use-src-plugin` is t. Otherwise, perform as
 `org-html-src-block`. CONTENTS holds the contents of the item.
 INFO is a plist used as a communication channel."
-  (if org-jekyll-use-src-plugin
+  (if org-wintersmith-use-src-plugin
       (let ((language (org-element-property :language src-block))
             (value (org-remove-indentation
                     (org-element-property :value src-block))))
@@ -125,17 +125,17 @@ INFO is a plist used as a communication channel."
     (org-export-with-backend 'html src-block contents info)))
 
 ;;; Template
-(defun org-jekyll-template (contents info)
+(defun org-wintersmith-template (contents info)
   "Return complete document string after HTML conversion.
 CONTENTS is the transcoded contents string. INFO is a plist
 holding export options."
-  (if org-jekyll-include-yaml-front-matter
+  (if org-wintersmith-include-yaml-front-matter
       (concat
-       (org-jekyll--yaml-front-matter info)
+       (org-wintersmith--yaml-front-matter info)
        contents)
     contents))
 
-(defun org-jekyll-inner-template (contents info)
+(defun org-wintersmith-inner-template (contents info)
   "Return body of document string after HTML conversion.
 CONTENTS is the transcoded contents string.  INFO is a plist
 holding export options."
@@ -144,7 +144,7 @@ holding export options."
    (let ((depth (plist-get info :with-toc)))
      (when depth (org-html-toc depth info)))
    ;; ;; PREVIEW mark on the top of article.
-   ;; (unless (equal "true" (plist-get info :jekyll-published))
+   ;; (unless (equal "true" (plist-get info :wintersmith-published))
    ;;   "<span style=\"background: red;\">PREVIEW</span>")
    ;; Document contents.
    contents
@@ -152,19 +152,19 @@ holding export options."
    (org-html-footnote-section info)))
 
 ;;; YAML Front Matter
-(defun org-jekyll--get-option (info property-name &optional default)
+(defun org-wintersmith--get-option (info property-name &optional default)
   (let ((property (org-export-data (plist-get info property-name) info)))
     (format "%s" (or property default ""))))
 
-(defun org-jekyll--yaml-front-matter (info)
+(defun org-wintersmith--yaml-front-matter (info)
   (let ((title
-         (org-jekyll--get-option info :title))
+         (org-wintersmith--get-option info :title))
         (date
-         (org-jekyll--get-option info :date))
+         (org-wintersmith--get-option info :date))
         (layout
-         (org-jekyll--get-option info :jekyll-layout org-jekyll-layout))
+         (org-wintersmith--get-option info :wintersmith-layout org-wintersmith-layout))
         (categories
-         (org-jekyll--get-option info :jekyll-categories org-jekyll-categories)))
+         (org-wintersmith--get-option info :wintersmith-categories org-wintersmith-categories)))
     (concat
      "---"
      "\ntitle: \""    title
@@ -174,14 +174,14 @@ holding export options."
      "\n---\n")))
 
 ;;; Filename and Date Helper
-(defun org-jekyll-date-from-filename (&optional filename)
+(defun org-wintersmith-date-from-filename (&optional filename)
   (let ((fn (file-name-nondirectory (or filename (buffer-file-name)))))
     (if (string-match "^[0-9]+-[0-9]+-[0-9]+" fn)
         (match-string 0 fn)
       nil)))
 
-(defun org-jekyll-property-list (&optional filename)
-  (let ((backend 'jekyll) plist)
+(defun org-wintersmith-property-list (&optional filename)
+  (let ((backend 'wintersmith) plist)
     (if filename
         (with-temp-buffer
           (insert-file-contents filename)
@@ -191,19 +191,19 @@ holding export options."
       (setq plist (org-export-backend-options backend))
       plist)))
 
-(defun org-jekyll-property (keys &optional filename)
-  (let ((plist (org-jekyll-property-list filename)))
-    (mapcar (lambda (key) (org-export-data-with-backend (plist-get plist key) 'jekyll plist))
+(defun org-wintersmith-property (keys &optional filename)
+  (let ((plist (org-wintersmith-property-list filename)))
+    (mapcar (lambda (key) (org-export-data-with-backend (plist-get plist key) 'wintersmith plist))
             keys)))
 
-(defun org-jekyll-date-from-property (&optional filename)
-  (let ((plist (org-jekyll-property filename)))
+(defun org-wintersmith-date-from-property (&optional filename)
+  (let ((plist (org-wintersmith-property filename)))
     (org-read-date
      nil nil
-     (org-export-data-with-backend (plist-get plist :date) 'jekyll plist))))
+     (org-export-data-with-backend (plist-get plist :date) 'wintersmith plist))))
 
-(defun org-jekyll-create-filename ()
-  (let ((date (org-jekyll-date-from-property))
+(defun org-wintersmith-create-filename ()
+  (let ((date (org-wintersmith-date-from-property))
         (file (file-name-nondirectory (buffer-file-name)))
         (dir  (file-name-directory (buffer-file-name))))
     (expand-file-name
@@ -212,29 +212,29 @@ holding export options."
 
 ;;; End-User functions
 ;;;###autoload
-(defun org-jekyll-export-subtree-as-html
+(defun org-wintersmith-export-subtree-as-html
     (&optional async visible-only body-only ext-plist)
   "Export current subtree to a HTML buffer adding some YAML front matter."
   (interactive)
   )
 
 ;;;###autoload
-(defun org-jekyll-export-as-html
+(defun org-wintersmith-export-as-html
     (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a HTML buffer adding some YAML front matter."
   (interactive)
   (if async
       (org-export-async-start
           (lambda (output)
-            (with-current-buffer (get-buffer-create "*Org Jekyll HTML Export*")
+            (with-current-buffer (get-buffer-create "*Org Wintersmith HTML Export*")
               (erase-buffer)
               (insert output)
               (goto-char (point-min))
               (funcall org-html-display-buffer-mode)
-              (org-export-add-to-stack (current-buffer) 'jekyll)))
-        `(org-export-as 'jekyll ,subtreep ,visible-only ,body-only ',ext-plist))
+              (org-export-add-to-stack (current-buffer) 'wintersmith)))
+        `(org-export-as 'wintersmith ,subtreep ,visible-only ,body-only ',ext-plist))
     (let ((outbuf (org-export-to-buffer
-                      'jekyll "*Org Jekyll HTML Export*"
+                      'wintersmith "*Org Wintersmith HTML Export*"
                     nil subtreep visible-only body-only ext-plist)))
       ;; Set major mode.
       (with-current-buffer outbuf (set-auto-mode t))
@@ -243,7 +243,7 @@ holding export options."
         outbuf))))
 
 ;;;###autoload
-(defun org-jekyll-export-to-html
+(defun org-wintersmith-export-to-html
     (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a HTML file adding some YAML front matter."
   (interactive)
@@ -252,17 +252,17 @@ holding export options."
          (org-export-coding-system org-html-coding-system))
     (if async
         (org-export-async-start
-            (lambda (f) (org-export-add-to-stack f 'jekyll))
+            (lambda (f) (org-export-add-to-stack f 'wintersmith))
           (let ((org-export-coding-system org-html-coding-system))
             `(expand-file-name
               (org-export-to-file
-                  'jekyll ,file ,subtreep ,visible-only ,body-only ',ext-plist))))
+                  'wintersmith ,file ,subtreep ,visible-only ,body-only ',ext-plist))))
       (let ((org-export-coding-system org-html-coding-system))
         (org-export-to-file
-            'jekyll file nil subtreep visible-only body-only ext-plist)))))
+            'wintersmith file nil subtreep visible-only body-only ext-plist)))))
 
 ;;;###autoload
-(defun org-jekyll-publish-to-html (plist filename pub-dir)
+(defun org-wintersmith-publish-to-html (plist filename pub-dir)
   "Publish an org file to HTML with YAML front matter.
 
 FILENAME is the filename of the Org file to be published.  PLIST
@@ -270,28 +270,28 @@ is the property list for the given project.  PUB-DIR is the
 publishing directory.
 
 Return output file name."
-  (org-publish-org-to 'jekyll filename ".html" plist pub-dir))
+  (org-publish-org-to 'wintersmith filename ".html" plist pub-dir))
 
 ;;;###autoload
-(defun org-jekyll-insert-export-options-template
+(defun org-wintersmith-insert-export-options-template
     (&optional title date setupfile categories published layout)
-  "Insert a settings template for Jekyll exporter."
+  "Insert a settings template for Wintersmith exporter."
   (interactive)
-  (let ((layout     (or layout org-jekyll-layout))
-        (published  (or published org-jekyll-published))
-        (categories (or categories org-jekyll-categories)))
+  (let ((layout     (or layout org-wintersmith-layout))
+        (published  (or published org-wintersmith-published))
+        (categories (or categories org-wintersmith-categories)))
     (save-excursion
       (insert (format (concat
                        "#+TITLE: "             title
                        "\n#+DATE: "              date
                        "\n#+SETUPFILE: "         setupfile
-                       "\n#+JEKYLL_LAYOUT: "     layout
-                       "\n#+JEKYLL_CATEGORIES: " categories
-                       "\n#+JEKYLL_PUBLISHED: "  published
+                       "\n#+WINTERSMITH_LAYOUT: "     layout
+                       "\n#+WINTERSMITH_CATEGORIES: " categories
+                       "\n#+WINTERSMITH_PUBLISHED: "  published
                        "\n\n* \n\n{{{more}}}"))))))
 
 ;;; provide
 
-(provide 'ox-jekyll)
+(provide 'ox-wintersmith)
 
-;;; ox-jekyll.el ends here
+;;; ox-wintersmith.el ends here
