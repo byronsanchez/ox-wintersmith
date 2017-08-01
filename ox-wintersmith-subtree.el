61aa1ce56cd299ad62600aa2614ca2f6b8c2d660
Byron Sanchez <byron@hackbytes.io>
2017-08-01 01:42:14 +0000

    Configure ox-wintersmith for my nitelite.io blog

--- ox-wintersmith-subtree.el
+++ ox-wintersmith-subtree.el
@@ -48,38 +48,80 @@
 (require 'ox-wintersmith)
 (require 'subr-x)
 
-(defcustom endless/blog-dir (expand-file-name "~/Git-Projects/blog/")
+(defcustom nitelite/blog-dir (expand-file-name "~/projects/byronsanchez/nitelite.io/contents/posts/")
   "Directory to save posts."
   :type 'directory
-  :group 'endless)
+  :group 'nitelite)
 
-(defcustom endless/blog-base-url "http://endlessparentheses.com/"
+(defcustom nitelite/blog-base-url "https://nitelite.io/"
   "Base URL of the blog.
 Will be stripped from links addresses on the final HTML."
   :type 'string
-  :group 'endless)
+  :group 'nitelite)
 
-(defun endless/export-to-blog (dont-show)
+(defun nitelite/org-export-all (backend blog-tag)
+  "Export all subtrees that are *not* tagged with :noexport: to
+separate files.
+
+Subtrees that do not have the :EXPORT_FILE_NAME: property set
+are exported to a filename derived from the headline text."
+    (save-excursion
+      ;; (set-mark (point-min))
+      ;;  (goto-char (point-max))
+      (org-map-entries
+       (lambda ()
+         (let ((export-file (org-entry-get (point) "EXPORT_FILE_NAME")))
+           (unless export-file
+             (org-set-property
+              "EXPORT_FILE_NAME"
+              ;; This function generates the export filename by replacing spaces
+              ;; with _ in the headline text.
+              ;;
+              ;; You can change =replace-regexp-in-string= to whatever you like
+              ;; (eg. your own handleize function)
+              (replace-regexp-in-string " " "_" (nth 4 (org-heading-components)))))
+           (funcall fn nil t)
+           (unless export-file (org-delete-property "EXPORT_FILE_NAME"))
+           (set-buffer-modified-p modifiedp)))
+       blog-tag nil)
+
+      ))
+
+(defun nitelite/export-nitelite (backend)
+  "Export all subtrees that are *not* tagged with :noexport: to
+separate files.
+
+Subtrees that do not have the :EXPORT_FILE_NAME: property set
+are exported to a filename derived from the headline text."
+  (interactive "sEnter backend: ")
+  (let ((fn (cond ((equal backend "html") 'org-html-export-to-html)
+                  ((equal backend "markdown") 'org-md-export-to-markdown)
+                  ((equal backend "latex") 'org-latex-export-to-latex)
+                  ((equal backend "pdf") 'org-latex-export-to-pdf)))
+        (modifiedp (buffer-modified-p)))
+    (nitelite/org-export-all backend "NITELITE & EXPORT")))
+
+(defun nitelite/export-to-blog (dont-show)
   "Exports current subtree as wintersmith html and copies to blog.
 Posts need very little to work, most information is guessed.
 Scheduled date is respected and heading is marked as DONE.
 
-Pages are marked by a \":EXPORT_WINTERSMITH_LAYOUT: page\" property,
+Pages are marked by a \":EXPORT_WINTERSMITH_TEMPLATE: page\" property,
 and they also need a :filename: property. Schedule is then
-ignored, and the file is saved inside `endless/blog-dir'.
+ignored, and the file is saved inside `nitelite/blog-dir'.
 
 The filename property is not mandatory for posts. If present, it
 will used exactly (no sanitising will be done). If not, filename
 will be a sanitised version of the title, see
-`endless/sanitise-file-name'."
+`nitelite/sanitise-file-name'."
   (interactive "P")
   (save-excursion
     ;; Actual posts NEED a TODO state. So we go up the tree until we
     ;; reach one.
     (while (null (org-entry-get (point) "TODO" nil t))
       (outline-up-heading 1 t))
-    (org-entry-put (point) "EXPORT_WINTERSMITH_LAYOUT"
-                   (org-entry-get (point) "EXPORT_WINTERSMITH_LAYOUT" t))
+    (org-entry-put (point) "EXPORT_WINTERSMITH_TEMPLATE"
+                   (org-entry-get (point) "EXPORT_WINTERSMITH_TEMPLATE" t))
     ;; Try the closed stamp first to make sure we don't set the front
     ;; matter to 00:00:00 which moves the post back a day
     (let* ((closed-stamp (org-entry-get (point) "CLOSED" t))
@@ -88,13 +130,13 @@ will be a sanitised version of the title, see
                    (org-get-scheduled-time (point) nil)))
            (tags (nreverse (org-get-tags-at)))
            (meta-title (org-entry-get (point) "meta_title"))
-           (is-page (string= (org-entry-get (point) "EXPORT_WINTERSMITH_LAYOUT") "page"))
+           (is-page (string= (org-entry-get (point) "EXPORT_WINTERSMITH_TEMPLATE") "page"))
            (name (org-entry-get (point) "filename"))
            (title (org-get-heading t t))
            (series (org-entry-get (point) "series" t))
            (org-wintersmith-categories
             (mapconcat
-             (lambda (tag) (endless/convert-tag tag))
+             (lambda (tag) (nitelite/convert-tag tag))
              tags " "))
            (org-export-show-temporary-export-buffer nil))
 
@@ -109,7 +151,7 @@ will be a sanitised version of the title, see
         ;; properties.
         ;; Define a name, if there isn't one.
         (unless name
-          (setq name (concat (format-time-string "%Y-%m-%d" date) "-" (endless/sanitise-file-name title)))
+          (setq name (concat (format-time-string "%Y-%m-%d" date) "-" (nitelite/sanitise-file-name title)))
           (org-entry-put (point) "filename" name))
         (org-todo 'done))
 
@@ -119,10 +161,10 @@ will be a sanitised version of the title, see
                (ignore-errors (ispell-buffer))
                (buffer-string)))
             (header-content
-             (endless/get-org-headers))
+             (nitelite/get-org-headers))
             (reference-buffer (current-buffer)))
         (with-temp-buffer
-          (endless/prepare-input-buffer
+          (nitelite/prepare-input-buffer
            header-content subtree-content reference-buffer)
 
           ;; Export and then do some fixing on the output buffer.
@@ -143,10 +185,10 @@ will be a sanitised version of the title, see
               (replace-match (concat " " (format-time-string "%Y-%m-%d %T" date)) :fixedcase :literal nil 1))
 
             ;; Save the final file.
-            (endless/clean-output-links)
+            (nitelite/clean-output-links)
             (let ((out-file
                    (expand-file-name (concat (if is-page "" "_posts/") name ".html")
-                                     endless/blog-dir)))
+                                     nitelite/blog-dir)))
               (write-file out-file)
               (unless dont-show
                 (find-file-other-window out-file)))
@@ -155,18 +197,18 @@ will be a sanitised version of the title, see
             (kill-new (concat "UPDATE: " title))
             (kill-new (concat "POST: " title))))))))
 
-(defun endless/get-org-headers ()
+(defun nitelite/get-org-headers ()
   "Return everything above the first headline of current buffer."
   (save-excursion
     (goto-char (point-min))
     (search-forward-regexp "^\\*+ ")
     (buffer-substring-no-properties (point-min) (match-beginning 0))))
 
-(defconst endless/base-regexp
-  (macroexpand `(rx (or ,endless/blog-base-url ,endless/blog-dir))))
+(defconst nitelite/base-regexp
+  (macroexpand `(rx (or ,nitelite/blog-base-url ,nitelite/blog-dir))))
 
-(defun endless/clean-output-links ()
-  "Strip `endless/blog-base-url' and \"file://\" from the start of URLs. "
+(defun nitelite/clean-output-links ()
+  "Strip `nitelite/blog-base-url' and \"file://\" from the start of URLs. "
   ;; Fix org's stupid filename handling.
   (goto-char (point-min))
   (while (search-forward-regexp "\\(href\\|src\\)=\"\\(file://\\)/" nil t)
@@ -174,12 +216,12 @@ will be a sanitised version of the title, see
   ;; Strip base-url from links
   (goto-char (point-min))
   (while (search-forward-regexp
-          (concat "href=\"" endless/base-regexp)
+          (concat "href=\"" nitelite/base-regexp)
           nil t)
     (replace-match "href=\"/" :fixedcase :literal))
   (goto-char (point-min)))
 
-(defun endless/prepare-input-buffer (header content reference-buffer)
+(defun nitelite/prepare-input-buffer (header content reference-buffer)
   "Insert content and clean it up a bit."
   (insert header content)
   (goto-char (point-min))
@@ -191,7 +233,7 @@ will be a sanitised version of the title, see
                   (not org-link-search-failed))
       (cond
        ((looking-at (format "\\[\\[\\(file:%s\\)"
-                            (regexp-quote (abbreviate-file-name endless/blog-dir))))
+                            (regexp-quote (abbreviate-file-name nitelite/blog-dir))))
         (replace-match "file:/" nil nil nil 1)
         (goto-char (match-beginning 0))
         (when (looking-at (rx "[[" (group "file:/images/" (+ (not space))) "]]"))
@@ -215,21 +257,21 @@ will be a sanitised version of the title, see
         (when (and target-filename
                    (null (string= target-filename this-filename)))
           (replace-match
-           (format "/%s.html" (endless/strip-date-from-filename target-filename))
+           (format "/%s.html" (nitelite/strip-date-from-filename target-filename))
            :fixedcase :literal nil 1))))))
   (goto-char (point-min))
   (outline-next-heading))
 
-(defun endless/strip-date-from-filename (name)
+(defun nitelite/strip-date-from-filename (name)
   (replace-regexp-in-string "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-" "" name))
 
-(defun endless/convert-tag (tag)
+(defun nitelite/convert-tag (tag)
   "Overcome org-mode's tag limitations."
   (replace-regexp-in-string
    "_" "-"
    (replace-regexp-in-string "__" "." tag)))
 
-(defun endless/sanitise-file-name (name)
+(defun nitelite/sanitise-file-name (name)
   "Make NAME safe for filenames.
 Removes any occurrence of parentheses (with their content),
 Trims the result,
